625f3016a38a0c6b67cbd3db032fe7be9aff6ce7
Anand Ahire <anandsahire8595@gmail.com>
2026-04-17 23:23:54 +0530

    fixed cors for production and dotenv setup

--- ExamNotesAI/server/index.js
+++ ExamNotesAI/server/index.js
@@ -11,12 +11,11 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors(
-    {origin:"http://localhost:5173",
-        credentials : true,
-        methods : ["GET", "POST", "PUT", "DELETE", "OPTIONS"] 
-    }
-))
+app.use(cors({
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}))
 
 app.use((req, res, next) => {
   res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
